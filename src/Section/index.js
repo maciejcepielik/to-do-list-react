e52d3c20bd1a40cfdec49051f537ca68bd609825
Maciej Cepielik <maciej.cepielik@gmail.com>
2024-04-14 20:55:00 +0200

    Changed class name from 'section__checkList' to 'section__title' in Section component

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,15 +1,15 @@
 import "./style.css";
 
 const Section = ({ title, body, extraHeaderContent }) => (
-    <section className="section">
-        <div className="section__checkList">
-            <div className="section__titleName">{title}</div>
-            {extraHeaderContent}
-        </div>
-        <div className="checkList">
-            {body}
-        </div>
-    </section>
+  <section className="section">
+    <div className="section__title">
+      <div className="section__titleName">{title}</div>
+      {extraHeaderContent}
+    </div>
+    <div className="checkList">
+      {body}
+    </div>
+  </section>
 );
 
 export default Section;
