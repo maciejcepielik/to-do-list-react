f2c7318ea0096e858168a358a72b8f471ec8c2ad
Maciej Cepielik <maciej.cepielik@gmail.com>
2024-04-14 20:01:09 +0200

    Moved the 'border: none' property from the 'section__title--checkList' class to 'section__title' and removed the 'section__title--checkList' class

--- src/Section/index.js
+++ src/Section/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 const Section = ({ title, body, extraHeaderContent }) => (
     <section className="section">
         <div className="section__checkList">
-            <div className="section__title section__title--checkList">{title}</div>
+            <div className="section__title">{title}</div>
             {extraHeaderContent}
         </div>
         <div className="checkList">
