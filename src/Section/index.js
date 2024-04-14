11f33da979008df16d06c562288b25aa8cf76617
Maciej Cepielik <maciej.cepielik@gmail.com>
2024-04-14 20:30:26 +0200

    Moved checkList class from style.css of Tasks component to style.css of Section component and changed class name from section__title to section__titleName

--- src/Section/index.js
+++ src/Section/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 const Section = ({ title, body, extraHeaderContent }) => (
     <section className="section">
         <div className="section__checkList">
-            <div className="section__title">{title}</div>
+            <div className="section__titleName">{title}</div>
             {extraHeaderContent}
         </div>
         <div className="checkList">
