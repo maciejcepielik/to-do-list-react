e3fdb49b53f15f8e7d553c6844549aadd542b6a2
Maciej Cepielik <maciej.cepielik@gmail.com>
2024-09-19 15:31:54 +0200

    create custom 'useTasks' hook for tasks menagement

--- src/App.js
+++ src/App.js
@@ -31,47 +31,66 @@ const storedTasks = () => {
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState(storedTasks);
-
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks))
-  }, [tasks]);
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone)
   };
 
-  const removeTask = (id) => {
-    setTasks(tasks => tasks.filter(task => task.id !== id))
+  const useTasks = () => {
+    const [tasks, setTasks] = useState(storedTasks);
+
+    useEffect(() => {
+      localStorage.setItem("tasks", JSON.stringify(tasks))
+    }, [tasks]);
+
+    const removeTask = (id) => {
+      setTasks(tasks => tasks.filter(task => task.id !== id))
+    };
+
+    const toggleTaskDone = (id) => {
+      setTasks(tasks => tasks.map(task => {
+        if (task.id === id) {
+          return { ...task, done: !task.done };
+        }
+
+        return task;
+      }));
+    };
+
+    const setAllDone = () => {
+      setTasks(tasks => tasks.map(task => ({
+        ...task,
+        done: true
+      })));
+    };
+
+    const addNewTask = (content) => {
+      setTasks(tasks => [
+        ...tasks,
+        {
+          content,
+          done: false,
+          id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+        }
+      ])
+    };
+
+    return {
+      tasks,
+      removeTask,
+      toggleTaskDone,
+      setAllDone,
+      addNewTask,
+    };
   };
 
-  const toggleTaskDone = (id) => {
-    setTasks(tasks => tasks.map(task => {
-      if (task.id === id) {
-        return { ...task, done: !task.done };
-      }
-
-      return task;
-    }));
-  };
-
-  const setAllDone = () => {
-    setTasks(tasks => tasks.map(task => ({
-      ...task,
-      done: true
-    })));
-  };
-
-  const addNewTask = (content) => {
-    setTasks(tasks => [
-      ...tasks,
-      {
-        content,
-        done: false,
-        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
-      }
-    ])
-  };
+  const {
+    tasks,
+    removeTask,
+    toggleTaskDone,
+    setAllDone,
+    addNewTask,
+  } = useTasks();
 
   return (
     <ThemeProvider theme={theme}>
