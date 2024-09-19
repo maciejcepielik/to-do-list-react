f13b92fff9caf14967bec61f0d4ff85244ecab1b
Maciej Cepielik <maciej.cepielik@gmail.com>
2024-09-19 15:37:29 +0200

    move 'initialTasks' to a separate file

--- src/App.js
+++ src/App.js
@@ -6,18 +6,13 @@ import Section from "./Section";
 import Header from "./Header";
 import Main from "./Main";
 import { ThemeProvider } from "styled-components";
+import { initialTasks } from "./initialTasks";
 
 const theme = {
   breakpoints: {
     mobile: 768
   },
 };
-
-const initialTasks = [
-  { id: 1, content: "wyprowadzić psa na spacer", done: true },
-  { id: 2, content: "pójść na trening", done: false },
-  { id: 3, content: "dokończyć projekt", done: false },
-];
 
 const storedTasks = () => {
   const localStorageTasks = localStorage.getItem('tasks');
---
+++ src/initialTasks.js
@@ -0,0 +1,5 @@
+export const initialTasks = [
+    { id: 1, content: "wyprowadzić psa na spacer", done: true },
+    { id: 2, content: "pójść na trening", done: false },
+    { id: 3, content: "dokończyć projekt", done: false },
+  ];
